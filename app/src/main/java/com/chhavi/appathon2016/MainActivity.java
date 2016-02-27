504807a80b01ad4f1595c84921a2cf347e70dc91
chhavip <coolaqua12@gmail.com>
2016-02-28 00:33:21 +0530

    Added speech to text but is faulty

--- app/src/main/java/com/chhavi/appathon2016/MainActivity.java
+++ app/src/main/java/com/chhavi/appathon2016/MainActivity.java
@@ -1,6 +1,8 @@
 package com.chhavi.appathon2016;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
@@ -9,16 +11,20 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private final int CHECK_CODE = 0x1;
     private final int LONG_DURATION = 5000;
     private final int SHORT_DURATION = 1200;
 
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
     TextToSpeech textToSpeech;
+    TextView txtSpeechInput;
    // private Speaker speaker;
 
     @Override
@@ -26,15 +32,17 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textToSpeech = new TextToSpeech(getApplicationContext(), this);
+        txtSpeechInput = (TextView)findViewById(R.id.speechinput);
      //   checkTTS();
 
+       // promptSpeechInput();
 
        // speaker.allow(true);
 
 
       //  speaker.speak(text);
 
-        textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+/*        textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
             @Override
             public void onStart(String utteranceId) {
                 Toast.makeText(MainActivity.this, "asdd", Toast.LENGTH_LONG).show();
@@ -50,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
             public void onError(String utteranceId) {
 
             }
-        });
+        });*/
     }
 
     private void checkTTS() {
@@ -111,8 +119,11 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                             public void run() {
                                 //UI changes
                                 Toast.makeText(MainActivity.this, "asdd", Toast.LENGTH_LONG).show();
+                                promptSpeechInput();
+
                             }
                         });
+
                     }
                 });
 
@@ -120,20 +131,49 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
         params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"stringId");
                // convertTextToSpeech("Hello and welcome, would you like to register as Volunteer or seak assistance? Say out loud Volunteer or Help according to your choice after the beep");
-                textToSpeech.speak("Hello and welcome, would you like to register as Volunteer or seak assistance? Say out loud Volunteer or Help according to your choice after the beep", TextToSpeech.QUEUE_FLUSH, params);
+             //   textToSpeech.speak("Hello and welcome, would you like to register as Volunteer or seak assistance? Say out loud Volunteer or Help according to your choice after the beep", TextToSpeech.QUEUE_FLUSH, params);
 
             }
 
 
-        }
-
-        else {
+        } else {
             Log.e("error", "Initilization Failed!");
         }
     }
 
-    @Override
-    public void onUtteranceCompleted(String utteranceId) {
 
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say Something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Speech not supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
