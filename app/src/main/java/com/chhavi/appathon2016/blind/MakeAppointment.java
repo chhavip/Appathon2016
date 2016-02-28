b18903ec09471aed7d829bfb499396f52662cdb9
chhavip <coolaqua12@gmail.com>
2016-02-28 11:53:58 +0530

    Added future appointments list

---
+++ app/src/main/java/com/chhavi/appathon2016/blind/MakeAppointment.java
@@ -0,0 +1,9 @@
+package com.chhavi.appathon2016.blind;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chhavi on 28/2/16.
+ */
+public class MakeAppointment extends AppCompatActivity {
+}
